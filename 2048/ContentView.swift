f9d09b0d8b0253c98f3ee1a8ec5e7aecb20016b6
jiangran <jiangran1997@yeah.net>
2020-06-08 11:10:44 +0800

    Initial Commit

---
+++ 2048/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  2048
+//
+//  Created by 姜冉 on 2020/6/8.
+//  Copyright © 2020 rain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
